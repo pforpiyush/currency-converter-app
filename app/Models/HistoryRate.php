9c7392b9134277b4e6084c09e7936767f233e99e
Piyush Gupta <piyush.gupta95@outlook.com>
2024-03-27 11:53:29 +1100

    Update env example file

--- app/Models/HistoryRate.php
+++ app/Models/HistoryRate.php
@@ -5,13 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Model class for history_rates table
+ */
 class HistoryRate extends Model
 {
-    public const LIMIT_DAILY = 10;
+    /**
+     * This sets the limit on number of records that can be fetched through external API
+     * 
+     * NOTE: The rate has been changed because only upto 10 requests can be made to the external API per minute.
+     * 
+     * For ideal situation, the rates would be as follows:
+     * - LIMIT_DAILY = 30;
+     * - LIMIT_WEEKLY = 26;
+     * - LIMIT_MONTHLY = 12;
+     */
+    public const LIMIT_DAILY = 10;      // For one month period and daily interval
 
-    public const LIMIT_WEEKLY = 6;
+    public const LIMIT_WEEKLY = 8;      // For six month period and weekly interval
 
-    public const LIMIT_MONTHLY = 4;
+    public const LIMIT_MONTHLY = 6;     // For one year period and monthly interval
 
     use HasFactory;
 
